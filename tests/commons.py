5f839ad6c2ddbe069a8eb1e344261b95975329de
Fabio Bove <fabio.bove.dr@gmail.com>
2024-11-15 22:17:31 +0100

    UPDATE: modified requirements, updated tests to skip CUDA dependent operation in case CUDA is not available.

--- tests/commons.py
+++ tests/commons.py
@@ -1,5 +1,8 @@
+import os
 from dataclasses import dataclass
 from enum import Enum
+
+base_dir = os.path.dirname(os.path.abspath(__file__))  # Directory containing common.py
 
 
 @dataclass
@@ -7,7 +10,7 @@ class TestDatasetFiles:
     test_real_sequences_limit = 10
 
     # DNA test dataset files: counts depends on sequences in the test file
-    dna_test_dataset_file = "./bio_test_samples/dna/dna_test_sequences.fna"
+    dna_test_dataset_file = os.path.join(base_dir, "./bio_test_samples/dna/dna_test_sequences.fna")
     dna_test_valid_sequences_count = 6
     dna_test_invalid_stop_sequences_count = 1
     dna_test_invalid_middle_stop_sequences_count = 1
@@ -15,7 +18,7 @@ class TestDatasetFiles:
     dna_test_invalid_start_sequences_count = 1
 
     # PROTEIN test dataset files: counts depends on sequences in the test file
-    protein_test_dataset_file = "./bio_test_samples/protein/protein_test_sequences.fna"
+    protein_test_dataset_file = os.path.join(base_dir, "./bio_test_samples/protein/protein_test_sequences.fna")
     protein_test_valid_sequences_count = 6
     protein_test_invalid_stop_sequences_count = 1
     protein_test_invalid_middle_stop_sequences_count = 1
@@ -23,7 +26,7 @@ class TestDatasetFiles:
     protein_test_invalid_start_sequences_count = 1
 
     # SMILES test dataset files: counts depends on sequences in the test file
-    smiles_test_dataset_file = "./bio_test_samples/smiles/smiles_test_sequences.fna"
+    smiles_test_dataset_file = os.path.join(base_dir, "./bio_test_samples/smiles/smiles_test_sequences.fna")
     smiles_test_valid_sequences_count = 6
     smiles_test_invalid_sequences_count = 3
     smiles_test_invalid_stop_sequences_count = 0
