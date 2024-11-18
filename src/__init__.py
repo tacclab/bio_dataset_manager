d8e3bff29ffb07dc83c407ac10dda37f33da5159
Fabio Bove <fabio.bove.dr@gmail.com>
2024-11-18 17:51:18 +0100

    REFACTOR: Usage of the lib, removed unused functionalities.

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,5 @@
-from .bio_dataset_manager.bio_dataloader import BioDataloader
-from .bio_dataset_manager.bio_dataset import BioDataset
-from .bio_sequences.bio_sequences_utils import BioSequencesUtils
-from .bio_sequences.dna_sequence import DnaSequence
-from .bio_sequences.protein_sequence import ProteinSequence
-from .bio_sequences.smiles_sequence import SmilesSequence
+from .bio_dataset_manager import BioDataloader, BioDataset
+from .bio_sequences import DnaSequence, ProteinSequence, SmilesSequence, BioSequencesUtils
 
 __all__ = [
     "BioSequencesUtils",
